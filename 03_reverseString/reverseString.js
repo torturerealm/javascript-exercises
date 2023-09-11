32dbe5e87d598f1177c9fe70246eff489a270764
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-11 16:23:40 +0800

    Update reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let newText = text.split("");
+    let reversedText = "";
+    for (i = text.length - 1; i >= 0; i--) {
+        reversedText += newText[i];
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
