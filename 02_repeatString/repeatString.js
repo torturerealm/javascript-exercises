126cc48fc5b9269e8ff06ac04550df5a53702985
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-11 15:26:45 +0800

    Update repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let tempString = "";
+    for (let i = 0; i < num; i++) {
+        tempString += string;
+    }
+    return tempString;
 };
 
 // Do not edit below this line
