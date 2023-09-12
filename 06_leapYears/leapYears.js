34baa324e80bc51cc3cafad94de620ca5d3c2faf
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-12 15:53:18 +0800

    Update leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 !== 0) return false;
+        return true;
+    } else return false; 
 };
+
+// if year is divisible by 4, leap year
+    // if year is divisible by 100, not leap year
+    // unless divisible by 400 too
+
 
 // Do not edit below this line
 module.exports = leapYears;
