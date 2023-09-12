f1e44a459c304940aa6618610cb00254c640e705
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-12 16:54:11 +0800

    Update tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let converted = (fahrenheit - 32) * (5 / 9);
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let converted = celsius * 1.8 + 32;
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
