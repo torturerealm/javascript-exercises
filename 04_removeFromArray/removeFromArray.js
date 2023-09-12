56c0ed2447566687016b40108a917b5121a50141
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-12 11:06:20 +0800

    Update removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //return = array.filter((arg) => !args.includes(arg));
 
+const newArray = [];
+array.forEach((item) => {
+    if (!args.includes(item)) {
+        newArray.push(item);
+    }
+});
+return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function() {
-
+// let sum be empty
+// for (let i be equals num1; increment until equals to num2) 
+// return sum
 };
 
 // Do not edit below this line
