26083b944ee3b5c1714e96f30b36df1af0e0165a
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-12 12:44:13 +0800

    Update sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,21 @@
-const sumAll = function() {
-// let sum be empty
-// for (let i be equals num1; increment until equals to num2) 
-// return sum
+const sumAll = function(num1, num2) {
+    let sum = 0;
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (isNaN(num1) || isNaN(num2)) {
+        return "ERROR";
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (num2 > num1) { 
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
