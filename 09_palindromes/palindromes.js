3bd19451ec260ea74e8654f60eeb4272b033e82e
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-10-16 10:16:41 +0800

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function(text) {
+    const punctuations = /[!., ]/g;
+    let newText = text.replace(punctuations, "");
+    newText = newText.toLowerCase();
 
+    let left = 0;
+    let right = newText.length - 1;
+    while (left < right) {
+        if (newText.charAt(left) != newText.charAt(right)) return false;
+        else {
+            left++;
+            right--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
