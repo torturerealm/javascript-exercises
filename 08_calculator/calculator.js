d2bb925d435aa57b33a1a7230c5966dd88457358
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-10-16 09:40:51 +0800

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.isArray) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    product = product * array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let product = 1;
+  for (let i = 0; i < b; i++) {
+    product = product * a;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	// multiplies itself after decrement
+  let product = 1;
+  for (let i = a; i > 0; i--) {
+    product = product * i;
+  }
+  return product;
 };
 
 // Do not edit below this line
